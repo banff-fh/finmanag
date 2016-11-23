84e2f8a14d7dddecfe13dcfdb9cf329976906213
W5OVEQOMS9LBBQE\Administrator <784838898@qq.com>
2016-11-23 18:22:55 +0800

    Added create_payment & audit_invoice Demo Layout .

--- fin_manager/cashier/urls.py
+++ fin_manager/cashier/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
      url(r'^find_invoice/', views.find_invoice, name='find_invoice'),
+     url(r'^audit_invoice/', views.audit_invoice, name='audit_invoice'),
+     url(r'^create_payment/', views.create_payment, name='create_payment'),
 ]
--- fin_manager/cashier/views.py
+++ fin_manager/cashier/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
-# Create your views here.
 
+# find invoice item layout
 def find_invoice(request):
     return render(request,'find_invoice.html',{})
 
+# audit invoice layout
+def audit_invoice(request):
+    return render(request,'audit_invoice.html',{})
+
+# create payment
+def create_payment(request):
+    return render(request,'create_payment.html',{})
